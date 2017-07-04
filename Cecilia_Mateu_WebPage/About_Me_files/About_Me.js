91e4cbefd1514db03bfae6a519c085a6949b3f4a
cmateu <cecilia.mateu@gmail.com>
2017-07-03 23:16:54 -0400

    cv added

--- Cecilia_Mateu_WebPage/About_Me_files/About_Me.js
+++ Cecilia_Mateu_WebPage/About_Me_files/About_Me.js
@@ -1,4 +1,4 @@
-// Created by iWeb 3.0.4 local-build-20170525
+// Created by iWeb 3.0.4 local-build-20170703
 
 setTransparentGifURL('Media/transparent.gif');function applyEffects()
 {var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWPhotoFrame([IWCreateImage('About_Me_files/NewTravel_C_TL.png'),IWCreateImage('About_Me_files/NewTravel_S_T.png'),IWCreateImage('About_Me_files/NewTravel_C_TR.png'),IWCreateImage('About_Me_files/NewTravel_S_R.png'),IWCreateImage('About_Me_files/NewTravel_C_BR.png'),IWCreateImage('About_Me_files/NewTravel_S_B.png'),IWCreateImage('About_Me_files/NewTravel_C_BL.png'),IWCreateImage('About_Me_files/NewTravel_S_L.png')],null,1,0.900000,71.000000,0.000000,62.000000,62.000000,83.000000,9.000000,73.000000,77.000000,8.000000,8.000000,8.000000,9.000000,null,null,null,0.400000)});registry.applyEffects();}
