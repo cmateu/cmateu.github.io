07e9f67914488a016e4dc89159dad973364b4f7b
cmateu <cecilia.mateu@gmail.com>
2017-11-06 15:07:33 +0100

    new webpage renamed and added

--- Cecilia_Mateu_WebPage/CatalinaGC3_Streams_files/CatalinaGC3_Streams.js
+++ Cecilia_Mateu_WebPage/CatalinaGC3_Streams_files/CatalinaGC3_Streams.js
@@ -3,7 +3,7 @@
 setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
 {return false;}
 function onPageLoad()
-{loadMozillaCSS('Catalina+GC3_Streams_files/Catalina+GC3_StreamsMoz.css')
+{loadMozillaCSS('CatalinaGC3_Streams_files/CatalinaGC3_StreamsMoz.css')
 adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');Widget.onload();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
 function onPageUnload()
 {Widget.onunload();}
